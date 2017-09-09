f2cd5799cc48539ff1ceeb1839b4dabcd2b5226b
Nazeef Mallick <nmallick@microsoft.com>
2017-09-09 11:56:28 -0700

    Implementing upload functionality for publish profile and centralizing message display

--- MultiConsoleExtension/Controllers/SettingsController.cs
+++ MultiConsoleExtension/Controllers/SettingsController.cs
@@ -8,6 +8,7 @@ using MultiConsoleExtension.Models;
 using Newtonsoft.Json.Linq;
 using System.IO;
 using Newtonsoft.Json;
+using System.Xml;
 
 namespace MultiConsoleExtension.Controllers
 {
@@ -39,42 +40,55 @@ namespace MultiConsoleExtension.Controllers
         }
 
 
-        public HttpResponseMessage Post(JObject siteSetting)
+        public HttpResponseMessage Post()
         {
-            if (siteSetting == null)
-            {
-                return Request.CreateResponse(HttpStatusCode.BadRequest,"POST body not found");
-            }
             SiteSettings siteSettings = new SiteSettings();
-            siteSettings.UserName = siteSetting.Value<string>("userName");
-            siteSettings.Password = siteSetting.Value<string>("password");
-            if (string.IsNullOrEmpty(siteSettings.UserName) || string.IsNullOrEmpty(siteSettings.Password))
+            if(System.Web.HttpContext.Current.Request.Files.Count > 0)
             {
-                return Request.CreateResponse(HttpStatusCode.BadRequest, "userName and password must have values in response body");
-            }
-            else {
-                var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(siteSettings.UserName + ":" + siteSettings.Password);
-                siteSettings.AuthHeader = "Basic " + System.Convert.ToBase64String(plainTextBytes);
-
-                //Save settings to a local file calles %home%\site\siteextensions\MultiConsoleExt\siteConnectionSettings.json
-
-                var rootPath = Environment.GetEnvironmentVariable("HOME"); // For use on Azure Websites
-                if (rootPath == null)
+                XmlDocument publishProfileDoc = new XmlDocument();
+                publishProfileDoc.Load(System.Web.HttpContext.Current.Request.Files[0].InputStream);
+                XmlNode publishProfile = publishProfileDoc.GetElementsByTagName("publishProfile").Item(0);
+                if (publishProfile.Attributes["userName"]!=null)
                 {
-                    rootPath = System.IO.Path.GetTempPath(); // For testing purposes
-                };
-                var userSettingsDir = Path.Combine(rootPath, @"site\siteextensions\MultiConsoleExt");
-                var userSettingsFile = userSettingsDir + @"\siteConnectionSettings.json";
-
-                if (!Directory.Exists(userSettingsDir))
-                {
-                    Directory.CreateDirectory(userSettingsDir);
+                    siteSettings.UserName = publishProfile.Attributes["userName"].Value;
                 }
 
-                File.WriteAllText(userSettingsFile, JsonConvert.SerializeObject(siteSettings));
-                
-                return Request.CreateResponse(HttpStatusCode.OK, siteSettings);
+                if (publishProfile.Attributes["userPWD"] != null)
+                {
+                    siteSettings.Password = publishProfile.Attributes["userPWD"].Value;
+                }
 
+                if(!string.IsNullOrEmpty(siteSettings.UserName) && !string.IsNullOrEmpty(siteSettings.Password))
+                {
+                    var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(siteSettings.UserName + ":" + siteSettings.Password);
+                    siteSettings.AuthHeader = "Basic " + System.Convert.ToBase64String(plainTextBytes);
+
+                    //Save settings to a local file calles %home%\site\siteextensions\MultiConsoleExt\siteConnectionSettings.json
+
+                    var rootPath = Environment.GetEnvironmentVariable("HOME"); // For use on Azure Websites
+                    if (rootPath == null)
+                    {
+                        rootPath = System.IO.Path.GetTempPath(); // For testing purposes
+                    };
+                    var userSettingsDir = Path.Combine(rootPath, @"site\siteextensions\MultiConsoleExt");
+                    var userSettingsFile = userSettingsDir + @"\siteConnectionSettings.json";
+
+                    if (!Directory.Exists(userSettingsDir))
+                    {
+                        Directory.CreateDirectory(userSettingsDir);
+                    }
+
+                    File.WriteAllText(userSettingsFile, JsonConvert.SerializeObject(siteSettings));
+                    return Request.CreateResponse(HttpStatusCode.OK, siteSettings);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "Unable to read usename / password from uploaded file");
+                }
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, "");
             }
         }
     }
--- MultiConsoleExtension/Scripts/controllers/consoleCtrl.js
+++ MultiConsoleExtension/Scripts/controllers/consoleCtrl.js
@@ -1,5 +1,41 @@
-    angular.module('MultiConsole', ['ngMaterial']).controller('consoleCtrl', function ($scope, $http) {
+angular.module('MultiConsole', ['ngMaterial']).controller('consoleCtrl', function ($scope, $http) {
 
+    $scope.showHideMessage = function (show, level, message) {
+        if (show) {
+            switch (level) {
+                case "Error":
+                    $("#checkMark").hide();
+                    $("#progressCircular").hide();
+                    $("#errorMark").show();
+                    break;
+
+                case "Success":                    
+                    $("#errorMark").hide();
+                    $("#progressCircular").hide();
+                    $("#checkMark").show();
+                    break;
+
+                case "Progress":                    
+                    $("#checkMark").hide();
+                    $("#errorMark").hide();
+                    $("#progressCircular").show();
+                    break;
+
+                default:
+                    break;
+            }
+            $scope.message = message;
+        }
+        else {
+            $("#errorMark").hide();
+            $("#checkMark").hide();
+            $("#progressCircular").hide();
+            $scope.message = "";
+        }
+        if (!$scope.$$phase){
+            $scope.$apply();
+        }
+    }
 
     $scope.getSiteConnectionSettings = function () {
         $http.get("/multiconsoleext/api/Settings").then(function (resp) {
@@ -11,25 +47,27 @@
                 //Start detecting instances after a while, let Angular complete its binding process
                 //Attempt to find instnaces only if the Auth settings are present, else the user should update username and password
                 $scope.consecutiveKnownInstancesFound = 0;
-                setTimeout($scope.callRemoteToGetInstanceDetailsAuto, 1500); 
+                $scope.showHideMessage(true, "Progress", "Looking for Instances.. " + $scope.hostingInstances.length + " instance(s) identified");
+                setTimeout($scope.callRemoteToGetInstanceDetailsAuto, 1500);
             }
-            
+
         }, function (x) {
-            alert("There was an error while retrieving the site settings, please provide your publish profile credentials again.");
+            $scope.showHideMessage(true, "Error", "There was an error while retrieving the site settings. Please upload the Publish Profile for this site.");
         });
     }
 
     $scope.init = function () {
         $("#tabsContainer").hide(); //Section of the page where console windows will be opened
         $("#connectToSettings").hide(); // Drop down to display a list of all the instances that were identified
-        $("#checkMark").hide();  //Check mark to be displayed once all instances have been identified
-        $("#progressCircular").hide(); //Progress bar indicating that the detection is in progress
-        $("#instanceCount").hide();  //Message displaying # of instances found
+
+        $scope.showHideMessage(false, "", "");
+        
         $("#btnRefresh").hide(); //Button to refresh list of instances
+
 
         $scope.hostingInstances = [];
         $scope.connectedInstances = [];
-        $scope.selectedHostingInstance = [];        
+        $scope.selectedHostingInstance = [];
 
         $scope.consecutiveKnownInstancesFoundLimit = 10; //This is because the extension only supports up to 10 instances hosting the site for perf reasons
         $scope.consecutiveKnownInstancesFound = 0;
@@ -42,12 +80,14 @@
     }
 
 
+
+
     //--------------AUTO IDENTIFY INSTANCES
 
     $scope.updateInstancesAuto = function (instanceInfo) {
         var alreadyExists = false;
         for (var i = 0; i < $scope.hostingInstances.length; i++) {
-            if ($scope.hostingInstances[i].InstanceId == instanceInfo.InstanceId) {                
+            if ($scope.hostingInstances[i].InstanceId == instanceInfo.InstanceId) {
                 alreadyExists = true;
                 $scope.consecutiveKnownInstancesFound++;
                 break;
@@ -55,17 +95,20 @@
         }
         if (!alreadyExists) {
             $scope.consecutiveKnownInstancesFound = 0;
-            $scope.hostingInstances.push(instanceInfo);            
+            $scope.hostingInstances.push(instanceInfo);
+            $scope.message = "Looking for Instances.. " + $scope.hostingInstances.length + " instance(s) identified";
         }
     }
 
 
-    $scope.callRemoteToGetInstanceDetailsAuto = function () {
-        $("#instanceCount").show();
-        if ($scope.consecutiveKnownInstancesFound < $scope.consecutiveKnownInstancesFoundLimit) {
-            $("#checkMark").hide();
+    $scope.callRemoteToGetInstanceDetailsAuto = function () {        
+        $scope.showHideMessage(true, "Progress", "Looking for Instances.. " + $scope.hostingInstances.length + " instance(s) identified");
+
+        if ($scope.consecutiveKnownInstancesFound < $scope.consecutiveKnownInstancesFoundLimit) {            
             $("#btnRefresh").hide();
-            $("#progressCircular").show();
+
+            $scope.showHideMessage(true, "Progress", "Looking for Instances.. " + $scope.hostingInstances.length + " instance(s) identified");
+            
             for (var i = 0; i <= $scope.consecutiveKnownInstancesFoundLimit; i++) {
                 callLocalWebAPI("hostname", "", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").done(function (resp) {
                     $scope.updateInstancesAuto({ MachineName: resp.Output.replace("\r\n", ""), InstanceId: resp.ARRAffinity, DisplayName: resp.Output.replace("\r\n", "") + " (" + resp.ARRAffinity.substring(0, 4) + ")" });
@@ -76,49 +119,21 @@
                 $("#connectToSettings").show();
                 //Perform cleanup here, the next time this function will not be called
                 //This is not guaranteed to execute only once though or even execute at all
-                alert($scope.hostingInstances.length + " instances found.");
+                $scope.showHideMessage(true, "Success", "Max detection attempts reached. " + $scope.hostingInstances.length + " instance(s) identified");                                
             }
             //Keep repeating every X seconds unless we have reached the max supported number of instances 
             setTimeout($scope.callRemoteToGetInstanceDetailsAuto, 3000);
         }
-        else {            
+        else {
             //Perform cleanup here, the next time this function will not be called
             //This is not guaranteed to execute only once
-            $("#progressCircular").hide();
-            $("#checkMark").show();
-            $("#btnRefresh").show();            
+            $scope.showHideMessage(true, "Success", $scope.hostingInstances.length + " instance(s) identified");
+            
+            $("#btnRefresh").show();
             $("#connectToSettings").css("display", "flex");  //Have to set this to flex else it is moving the button on next line
         }
     }
     //--------------AUTO IDENTIFY INSTANCES
-
-    $scope.getAndUpdateInstanceDetails = function () {
-        var req = {
-            method: 'POST',
-            url: '/multiconsoleext/api/Settings',
-            headers: {
-                'Content-Type': 'text/json'
-            },
-            data: JSON.stringify({ userName: $scope.publishProfileUserName, password: $scope.publishProfilePassword })
-        }
-
-        $http(req).then(function (resp) {
-            $scope.publishProfileUserName = resp.data.UserName;
-            $scope.publishProfilePassword = resp.data.Password;
-            authHeader = resp.data.AuthHeader;
-            if (authHeader != "") {
-                $scope.hostingInstances = [];
-                $scope.connectedInstances = [];
-                $scope.selectedHostingInstance = [];
-                $scope.consecutiveKnownInstancesFound = 0;
-                $scope.callRemoteToGetInstanceDetailsAuto();
-            }
-            else {
-                alert("There was an error retrieving the saved connection info.");
-            }
-
-        }, function () { alert("There was an error while saving the settings"); });
-    }
 
     $scope.refreshInstanceDetails = function () {
         if (authHeader != "") {
@@ -126,10 +141,11 @@
             $scope.connectedInstances = [];
             $scope.selectedHostingInstance = [];
             $scope.consecutiveKnownInstancesFound = 0;
+            $scope.showHideMessage(true, "Progress", "Looking for Instances.. " + $scope.hostingInstances.length + " instance(s) identified");
             $scope.callRemoteToGetInstanceDetailsAuto();
         }
         else {
-            alert("There was an error retrieving the saved connection info.");
+            $scope.showHideMessage(true, "Error", "There was an error retrieving the saved connection info.");            
         }
     }
 
@@ -147,7 +163,7 @@
         $scope.connectedInstances = [];
 
         for (var i = 0; i < $scope.selectedHostingInstance.length; i++) {
-            console.log($scope.selectedHostingInstance[i]);
+            //console.log($scope.selectedHostingInstance[i]);
             $scope.connectedInstances.push({
                 MachineName: $scope.selectedHostingInstance[i].MachineName,
                 InstanceId: $scope.selectedHostingInstance[i].InstanceId,
@@ -158,6 +174,49 @@
         }
 
         LoadConsole($scope.connectedInstances);
+    }
+
+    $scope.showUploadDialog = function () {
+        $('#upldPublishProfile').click();
+    }
+
+    $scope.uploadPublishProfile = function (upldCtrl) {
+        
+        //console.log(upldCtrl.files[0]);
+        if (upldCtrl.files.length > 0) {
+            //Make sure that the file that is being uploaded in a PublishSettings file
+            if (upldCtrl.files[0].name.split(".")[1] != "PublishSettings") {                
+                $scope.showHideMessage(true, "Error", "Please upload the PublishSettings file.");
+                return false;
+            }
+            
+            $scope.showHideMessage(true, "Progress", "Uploading Publish Profile..");
+
+            var file = upldCtrl.files[0];
+            var data = new FormData();
+            data.append('file', file);
+
+            var uploadUrl = "/multiconsoleext/api/Settings";
+            $http.post(uploadUrl, data, {
+                withCredentials: true,
+                headers: { 'Content-Type': undefined },
+                transformRequest: angular.identity
+            }).success(function (resp) {
+                //Set the authHeader here as resp.AuthHeader
+                authHeader = resp.AuthHeader;
+
+                //Now that we have the AuthHeader, start looking for instances now...
+                $scope.consecutiveKnownInstancesFound = 0;
+                setTimeout($scope.callRemoteToGetInstanceDetailsAuto, 1500);
+                
+                $scope.showHideMessage(true, "Success", "Publish Profile sucessfully uploaded.");
+                
+            }).error(function () {
+                //Clear the state of hte file upload control so as to allow the user a chance to upload again.                
+                $scope.showHideMessage(true, "Error", "There was an error saving the Publish Profile.");
+            });
+        }
+        return false;
     }
 
 
