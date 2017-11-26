a99b4fb46f69235fc161babe71053adb4cbc15b9
Nazeef Mallick <nmallick@microsoft.com>
2017-11-25 17:49:07 -0800

    Adding the FileSystemHelper class

---
+++ MultiConsoleExtension/App_Code/FileSystemHelper.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace MultiConsoleExtension.App_Code
+{
+    public static class FileSystemHelper
+    {
+        public static void EnsureFolderStructure()
+        {
+            if (!Directory.Exists(RootPath))
+            {
+                Directory.CreateDirectory(RootPath);
+            }
+
+            foreach (string action in Enum.GetNames(typeof(ProxyAction)))
+            {
+                if(!Directory.Exists(DirectoryPathFor((ProxyAction)Enum.Parse(typeof(ProxyAction), action))))
+                {
+                    Directory.CreateDirectory(DirectoryPathFor((ProxyAction)Enum.Parse(typeof(ProxyAction), action)));
+                }                
+            }
+        }
+
+        public static string RootPath
+        {
+            get {
+                var _rootPath = Environment.GetEnvironmentVariable("HOME"); // For use on Azure Websites
+                if (_rootPath == null)
+                {
+                    _rootPath = System.IO.Path.GetTempPath(); // For testing purposes
+                };
+                return Path.Combine(_rootPath, @"site\siteextensions\InstanceDetective");
+            }
+        }
+
+        public  enum ProxyAction {
+            FullDump,
+            MiniDump,
+            ProfilerTrace,
+            ProfilerReport
+        }
+
+        public static string DirectoryPathFor(ProxyAction action) {
+            return RootPath + @"\" + action;
+        }
+
+        public static string UserSettingsFileName { get { return "siteConnectionSettings.json"; } }
+        public static string UserSettingsFileFullPath { get { return RootPath + @"\" + UserSettingsFileName; } }
+
+    }
+}
