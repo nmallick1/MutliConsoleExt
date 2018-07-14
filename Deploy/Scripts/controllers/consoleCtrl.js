b89e674f75143122c39aca81c3a5a7b9b6c20014
Nazeef Mallick <nmallick@microsoft.com>
2018-07-13 18:18:42 -0700

    Trace Analysis Controller is not done yet. Will also have to add UI for it.

--- Deploy/Scripts/controllers/consoleCtrl.js
+++ Deploy/Scripts/controllers/consoleCtrl.js
@@ -1,119 +1,170 @@
-angular.module('MultiConsole', ['ngMaterial']).controller('consoleCtrl', function ($scope, $http) {
+
+angular.module('MultiConsole', ['ngMaterial', 'directive.contextMenu']).controller('consoleCtrl', function ($scope, $http) {
 
 
-    $scope.getSiteConnectionSettings = function () {
-        $http.get("/multiconsoleext/api/Settings").then(function (resp) {
+
+
+
+    $scope.showHideMessage = function (show, level, message) {
+        if (show) {
+            switch (level) {
+                case "Error":
+                    $("#checkMark").hide();
+                    $("#progressCircular").hide();
+                    $("#errorMark").show();
+                    break;
+
+                case "Success":                    
+                    $("#errorMark").hide();
+                    $("#progressCircular").hide();
+                    $("#checkMark").show();
+                    break;
+
+                case "Progress":                    
+                    $("#checkMark").hide();
+                    $("#errorMark").hide();
+                    $("#progressCircular").show();
+                    break;
+
+                default:
+                    break;
+            }
+            $scope.message = message;
+        }
+        else {
+            $("#errorMark").hide();
+            $("#checkMark").hide();
+            $("#progressCircular").hide();
+            $scope.message = "";
+        }
+        if (!$scope.$$phase){
+            $scope.$apply();
+        }
+    }
+
+    $scope.detectInstances = function () {
+        //Start detecting instances after a while, let Angular complete its binding process
+        //Attempt to find instnaces only if the Auth settings are present, else the user should update username and password
+        $scope.consecutiveKnownInstancesFound = 0;
+        $scope.showHideMessage(true, "Progress", "Looking for Instances.. " + $scope.hostingInstances.length + " instance(s) identified");
+        setTimeout($scope.callRemoteToGetInstanceDetailsAuto, 1500);
+    }
+
+    $scope.getSiteConnectionSettingsAndDetectInstances = function () {
+        $http.get("/InstanceDetective/api/Settings").then(function (resp) {
             $scope.publishProfileUserName = resp.data.UserName;
             $scope.publishProfilePassword = resp.data.Password;
             authHeader = resp.data.AuthHeader;
+
+            if (authHeader != "") {
+                $scope.detectInstances();
+            }
+
         }, function (x) {
-            alert("There was an error while retrieving the site settings, please provide your publish profile credentials again.");
+            $scope.showHideMessage(true, "Error", "There was an error while retrieving the site settings. Please upload the Publish Profile for this site.");
         });
     }
 
     $scope.init = function () {
-        $("#tabsContainer").hide();
-        $("#connectToSettings").hide();
-        $("#instanceCount").hide();
+        $("#tabsContainer").hide(); //Section of the page where console windows will be opened
+        $("#connectToSettings").hide(); // Drop down to display a list of all the instances that were identified
+
+        $scope.showHideMessage(false, "", "");
+        
+        $("#btnRefresh").hide(); //Button to refresh list of instances
+
 
         $scope.hostingInstances = [];
         $scope.connectedInstances = [];
         $scope.selectedHostingInstance = [];
 
-        $scope.maxRetryAttemptsToGetInstanceDetails = 5;
-        $scope.currRetryAttemptsToGetInstanceDetails = 1;
+        $scope.consecutiveKnownInstancesFoundLimit = 10; //This is because the extension only supports up to 10 instances hosting the site for perf reasons
+        $scope.consecutiveKnownInstancesFound = 0;
 
 
         $scope.publishProfileUserName = "";
         $scope.publishProfilePassword = "";
 
-        $scope.getSiteConnectionSettings();
+        
+        if (authHeader != "")
+        {
+            //This will execute only when authHeader is hardcoded during testing
+            $scope.detectInstances();
+        }
+        else
+        {
+            $scope.getSiteConnectionSettingsAndDetectInstances();
+        }
     }
 
-    $scope.init();
 
-   
 
-    $scope.updateInstances = function (instanceInfo) {
+
+    //--------------AUTO IDENTIFY INSTANCES
+
+    $scope.updateInstancesAuto = function (instanceInfo) {
         var alreadyExists = false;
         for (var i = 0; i < $scope.hostingInstances.length; i++) {
             if ($scope.hostingInstances[i].InstanceId == instanceInfo.InstanceId) {
                 alreadyExists = true;
-            }
-            if (alreadyExists) break;
-        }
-        if (!alreadyExists) {
-            if ($scope.hostingInstances.length < parseInt($('#siteInstances').val())) {
-                $scope.hostingInstances.push(instanceInfo);
-            }
-            if ($scope.hostingInstances.length == parseInt($('#siteInstances').val())) {
-                //$("#instanceCount").hide();
-            }
-        }
-
-    }
-
-
-
-    $scope.callRemoteToGetInstanceDetails = function (instanceCount) {
-        $scope.currRetryAttemptsToGetInstanceDetails++;
-        for (var i = 0; i < instanceCount + 5; i++) {
-            if ($scope.hostingInstances.length == instanceCount) {
-                clearInterval($scope.callRemoteToGetInstanceDetailsHandle);
-                //$("#instanceCount").hide();
+                $scope.consecutiveKnownInstancesFound++;
                 break;
             }
-            callLocalWebAPI("hostname", "", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").done(function (resp) {
-                $scope.updateInstances({ MachineName: resp.Output.replace("\r\n", ""), InstanceId: resp.ARRAffinity, DisplayName: resp.Output.replace("\r\n", "") + " (" + resp.ARRAffinity.substring(0, 4) + ")" });
-                $scope.$apply();
-            });
         }
-        if (($scope.currRetryAttemptsToGetInstanceDetails == $scope.maxRetryAttemptsToGetInstanceDetails) || ($scope.hostingInstances.length == instanceCount)) {
-            clearInterval($scope.callRemoteToGetInstanceDetailsHandle);
-            //$("#instanceCount").hide();
-            if (($scope.currRetryAttemptsToGetInstanceDetails == $scope.maxRetryAttemptsToGetInstanceDetails) && ($scope.hostingInstances.length < instanceCount)) {
-                alert($scope.hostingInstances.length + " out of " + instanceCount + " instances found. Max retry attempts of " + $scope.maxRetryAttemptsToGetInstanceDetails + " reached.");
-            }
+        if (!alreadyExists) {
+            $scope.consecutiveKnownInstancesFound = 0;
+            $scope.hostingInstances.push(instanceInfo);
+            $scope.message = "Looking for Instances.. " + $scope.hostingInstances.length + " instance(s) identified";
         }
     }
 
-    $scope.getAndUpdateInstanceDetails = function () {
 
+    $scope.callRemoteToGetInstanceDetailsAuto = function () {        
+        $scope.showHideMessage(true, "Progress", "Looking for Instances.. " + $scope.hostingInstances.length + " instance(s) identified");
 
+        if ($scope.consecutiveKnownInstancesFound < $scope.consecutiveKnownInstancesFoundLimit) {            
+            $("#btnRefresh").hide();
 
-
-        var req = {
-            method: 'POST',
-            url: '/multiconsoleext/api/Settings',
-            headers: {
-                'Content-Type': 'text/json'
-            },
-            data: JSON.stringify({ userName: $scope.publishProfileUserName, password: $scope.publishProfilePassword })
-        }
-
-        $http(req).then(function (resp) {
-
-
-            $scope.publishProfileUserName = resp.data.UserName;
-            $scope.publishProfilePassword = resp.data.Password;
-            authHeader = resp.data.AuthHeader;
-
-            var instanceCount = parseInt($('#siteInstances').val());
-            if (instanceCount < 1) {
-                alert("Site must run on atleast one instance. Please set '# Instances this site runs on' appropriately.");
+            $scope.showHideMessage(true, "Progress", "Looking for Instances.. " + $scope.hostingInstances.length + " instance(s) identified");
+            
+            for (var i = 0; i <= $scope.consecutiveKnownInstancesFoundLimit; i++) {
+                callLocalWebAPI("hostname", "", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa").done(function (resp) {
+                    $scope.updateInstancesAuto({ MachineName: resp.Output.replace("\r\n", ""), InstanceId: resp.ARRAffinity, DisplayName: resp.Output.replace("\r\n", "") + " (" + resp.ARRAffinity.substring(0, 4) + ")" });
+                    $scope.$apply();
+                });
             }
-            else {
-                $scope.hostingInstances = [];                
+            if ($scope.consecutiveKnownInstancesFound >= $scope.consecutiveKnownInstancesFoundLimit) {
                 $("#connectToSettings").show();
-                $("#instanceCount").show();
-                $scope.callRemoteToGetInstanceDetails(instanceCount);
-                $scope.callRemoteToGetInstanceDetailsHandle = setInterval($scope.callRemoteToGetInstanceDetails, 5000, instanceCount);
+                //Perform cleanup here, the next time this function will not be called
+                //This is not guaranteed to execute only once though or even execute at all
+                $scope.showHideMessage(true, "Success", "Max detection attempts reached. " + $scope.hostingInstances.length + " instance(s) identified");                                
             }
-        }, function () { alert("There was an error while saving the settings"); });
+            //Keep repeating every X seconds unless we have reached the max supported number of instances 
+            setTimeout($scope.callRemoteToGetInstanceDetailsAuto, 3000);
+        }
+        else {
+            //Perform cleanup here, the next time this function will not be called
+            //This is not guaranteed to execute only once
+            $scope.showHideMessage(true, "Success", $scope.hostingInstances.length + " instance(s) identified");
+            
+            $("#btnRefresh").show();
+            $("#connectToSettings").css("display", "flex");  //Have to set this to flex else it is moving the button on next line
+        }
+    }
+    //--------------AUTO IDENTIFY INSTANCES
 
-
-
-
+    $scope.refreshInstanceDetails = function () {
+        if (authHeader != "") {
+            $scope.hostingInstances = [];
+            $scope.connectedInstances = [];
+            $scope.selectedHostingInstance = [];
+            $scope.consecutiveKnownInstancesFound = 0;
+            $scope.showHideMessage(true, "Progress", "Looking for Instances.. " + $scope.hostingInstances.length + " instance(s) identified");
+            $scope.callRemoteToGetInstanceDetailsAuto();
+        }
+        else {
+            $scope.showHideMessage(true, "Error", "There was an error retrieving the saved connection info.");            
+        }
     }
 
 
@@ -130,18 +181,65 @@ angular.module('MultiConsole', ['ngMaterial']).controller('consoleCtrl', functio
         $scope.connectedInstances = [];
 
         for (var i = 0; i < $scope.selectedHostingInstance.length; i++) {
-            console.log($scope.selectedHostingInstance[i]);
+            //console.log($scope.selectedHostingInstance[i]);
             $scope.connectedInstances.push({
                 MachineName: $scope.selectedHostingInstance[i].MachineName,
                 InstanceId: $scope.selectedHostingInstance[i].InstanceId,
                 DisplayName: $scope.selectedHostingInstance[i].DisplayName,
                 cmdPromptContainerId: "cmdPromptContainer" + i,
-                kuduExecConsoleId: "KuduExecConsoleTest" + i
+                kuduExecConsoleId: "KuduExecConsole" + i
             });
         }
 
         LoadConsole($scope.connectedInstances);
-
+        profilerInit($scope, $http);
     }
+
+    $scope.showUploadDialog = function () {
+        $('#upldPublishProfile').click();
+    }
+
+    $scope.uploadPublishProfile = function (upldCtrl) {
+        
+        //console.log(upldCtrl.files[0]);
+        if (upldCtrl.files.length > 0) {
+            //Make sure that the file that is being uploaded in a PublishSettings file
+            if (upldCtrl.files[0].name.split(".")[1] != "PublishSettings") {                
+                $scope.showHideMessage(true, "Error", "Please upload the PublishSettings file.");
+                return false;
+            }
+            
+            $scope.showHideMessage(true, "Progress", "Uploading Publish Profile..");
+
+            var file = upldCtrl.files[0];
+            var data = new FormData();
+            data.append('file', file);
+
+            var uploadUrl = "/InstanceDetective/api/Settings";
+            $http.post(uploadUrl, data, {
+                withCredentials: true,
+                headers: { 'Content-Type': undefined },
+                transformRequest: angular.identity
+            }).success(function (resp) {
+                //Set the authHeader here as resp.AuthHeader
+                authHeader = resp.AuthHeader;
+
+                //Now that we have the AuthHeader, start looking for instances now...
+                $scope.consecutiveKnownInstancesFound = 0;
+                setTimeout($scope.callRemoteToGetInstanceDetailsAuto, 1500);
+                
+                $scope.showHideMessage(true, "Success", "Publish Profile sucessfully uploaded.");
+                
+            }).error(function () {
+                //Clear the state of hte file upload control so as to allow the user a chance to upload again.                
+                $scope.showHideMessage(true, "Error", "There was an error saving the Publish Profile.");
+            });
+        }
+        return false;
+    }
+
+
+    $scope.init();
+
 
 });
---
+++ MultiConsoleExtension/Controllers/TraceAnalysisController.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MultiConsoleExtension.App_Code;
+using System.IO;
+using System.Diagnostics;
+
+namespace MultiConsoleExtension.Controllers
+{
+    public class TraceAnalysisController : ApiController
+    {
+        public HttpResponseMessage Post()
+        {
+            //When you submit a request for analysis, also return the ARRAffinityID of the instance where the analysis is being executed. It will help check for the status of this PID
+            //Start a new Process on current instance with the command line D:\Program Files (x86)\SiteExtensions\DaaS\<<LatestVersion>>\bin\DiagnosticTools\clrprofiler\CLRProfilingAnalyzer.exe "c:\sometime.diagsession" "d:\output" and return the current ARRID and PID
+            //To find the latest version, look for files of extension *.installed. Pick up the latest file. The folder name is the same as the name of file minus the file extension. This will be your latest installed analyzer
+
+
+            if (Path.GetExtension(System.Web.HttpContext.Current.Request.Files[0].FileName).ToLower() == "diagsession")
+            {
+                #region Get path to the latest installed version of CLRProfilingAnalyzer.exe
+                DirectoryInfo dirInfo = new DirectoryInfo(@"D:\Program Files (x86)\SiteExtensions\DaaS\");
+                string analyzerEXEPath = dirInfo.GetFiles("*.installed", SearchOption.TopDirectoryOnly).OrderByDescending(f => f.LastWriteTime).FirstOrDefault().FullName;
+                analyzerEXEPath = analyzerEXEPath.ToLower().Replace(".installed", "") + @"\bin\DiagnosticTools\clrprofiler\CLRProfilingAnalyzer.exe";
+                #endregion
+
+                FileSystemHelper.EnsureFolderStructure();
+                string traceSaveFolder = Path.Combine(FileSystemHelper.DirectoryPathFor(FileSystemHelper.ProxyAction.ProfilerReport), System.DateTime.Now.ToString("yyyy-MM-dd_hh-mm-ss-fffff"));
+                string fullTracePath = Path.Combine(traceSaveFolder, System.Web.HttpContext.Current.Request.Files[0].FileName);
+                Directory.CreateDirectory(traceSaveFolder);
+                System.Web.HttpContext.Current.Request.Files[0].SaveAs(fullTracePath);
+
+
+                if (Request.RequestUri.Host.ToLower() != "localhost")
+                {
+                    Process traceAnalyzerProcess = new Process();
+                    traceAnalyzerProcess.StartInfo = new ProcessStartInfo(analyzerEXEPath, "\"" + fullTracePath + "\" \"" + traceSaveFolder + "\"");
+                    traceAnalyzerProcess.Start();
+                    string curAffinityId = Environment.GetEnvironmentVariable("WEBSITE_INSTANCE_ID");
+                    int PID = traceAnalyzerProcess.Id;
+                    string responseString = "https://" + Request.RequestUri.Host + "/InstanceDetective/api/TraceAnalysis/" + PID + "/" + curAffinityId;
+                    return Request.CreateResponse(HttpStatusCode.OK, responseString);
+                }
+                else
+                {
+                    //This part will run only when I am testing the app locally
+                    //Intentionally performing this test to check for localhost in the URL later so that I can test file save and rest of the logic locally
+                    return Request.CreateErrorResponse(HttpStatusCode.BadRequest, new Exception("Test this only on Azure"));
+                }
+
+            }
+            else
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "File should be a .diagsession file");
+            }            
+        }
+
+
+        [Route("api/TraceAnalysis/{PID}/{ArrAffinity}")]
+        public HttpResponseMessage Get(int PID, string ArrAffinity)
+        {
+            return Request.CreateErrorResponse(HttpStatusCode.InternalServerError, new Exception("Yet to implement this. Look for PID : " + PID + " on Instance : " + ArrAffinity));
+        }
+    }
+}
